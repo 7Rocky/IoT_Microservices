3c9b1ffc23b92b5abd7e92d700053b580532ef09
Rocky <rockygitt@gmail.com>
2020-04-09 17:52:17 +0200

    Singleton for getting rabbitmq connection

---
+++ stats-ms/src/rabbitmq.py
@@ -0,0 +1,16 @@
+import pika
+
+from config import RABBITMQ_HOST, RABBITMQ_PASSWORD, RABBITMQ_PORT, RABBITMQ_USERNAME
+
+channel = None
+
+def get_channel():
+    global channel
+
+    if channel is None:
+        credentials = pika.PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(RABBITMQ_HOST, RABBITMQ_PORT, '/', credentials))
+        channel = connection.channel()
+
+    return channel
