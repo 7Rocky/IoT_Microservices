ff3f6a1bd8e2b61872b5f970cf088df643a187a5
Rocky <rockygitt@gmail.com>
2020-04-09 17:50:35 +0200

    Added env variable DB_NAME

--- temperature-ms/src/constants/constants.js
+++ temperature-ms/src/constants/constants.js
@@ -1,6 +1,7 @@
 module.exports = {
   ARDUINO_IP: process.env.ARDUINO_IP || 'localhost',
   B_TERMISTOR: 4275,
+  DB_NAME: process.env.MONGO_DATABASE_NAME || 'iot',
   PORT: process.env.PORT || 4000,
   REFRESH_TIME: 60000
 };
