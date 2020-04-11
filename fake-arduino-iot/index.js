602b2a46a750e5548093ad4c2a2e187157125593
Rocky <rockygitt@gmail.com>
2020-04-11 19:28:43 +0200

    Fixed fake responses

--- fake-arduino-iot/index.js
+++ fake-arduino-iot/index.js
@@ -9,11 +9,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/temperature', (req, res) => {
-  res.json({ temperature: [ Number((Math.random() * 100 + 400).toFixed()) ] });
+  res.json({ temperature: Number((Math.random() * 100 + 400).toFixed()) });
 });
 
 app.get('/humidity', (req, res) => {
-  res.json({ humidity: [ Number((Math.random() * 100 + 400).toFixed()) ] });
+  res.json({ humidity: Number((Math.random() * 100 + 400).toFixed()) });
 });
 
 app.listen(PORT, () => console.log('Fake Arduino at http://localhost:' + PORT));
