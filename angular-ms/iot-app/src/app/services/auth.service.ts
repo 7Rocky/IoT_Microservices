9340c2fe6c0959c927b6ded114553db86650aa33
Rocky <rockygitt@gmail.com>
2020-04-29 02:46:04 +0200

    refresh token and auth issues

--- angular-ms/iot-app/src/app/services/auth.service.ts
+++ angular-ms/iot-app/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
@@ -10,6 +10,16 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AuthService {
+
+  private logged = new Subject<boolean>();
+
+  logInAnnounced$ = this.logged.asObservable();
+  isLoggedIn: boolean = !!localStorage.getItem('iot-ms-token');
+  username: string = '';
+
+  announceLogIn(logged: boolean) {
+    this.logged.next(logged);
+  }
 
   constructor(
     private http: HttpClient
@@ -24,10 +34,12 @@ export class AuthService {
         }
       )
       .pipe(
-        tap((response: { token: string }) => {
-          localStorage.setItem('iot-ms-token', response.token);
-          localStorage.setItem('iot-ms-user', username);
-        })
+        tap(
+          (response: { refreshToken: string, token: string }) => {
+            this.setTokens(response.refreshToken, response.token, username)
+          },
+          () => this.removeTokens()
+        )
       );
   }
 
@@ -40,11 +52,69 @@ export class AuthService {
         }
       )
       .pipe(
-        tap((response: { token: string }) => {
-          localStorage.setItem('iot-ms-token', response.token);
-          localStorage.setItem('iot-ms-user', username);
-        })
+        tap(
+          (response: { refreshToken: string, token: string }) => {
+            this.setTokens(response.refreshToken, response.token, username)
+          },
+          () => this.removeTokens()
+        )
       );
+  }
+
+  refresh(refreshToken: string): Promise<any> {
+    return this.http.post<any>(
+        `http://${environment.ORCHESTRATOR_MS}/refresh`,
+        { 
+          refreshToken
+        }
+      )
+      .pipe(
+        tap(
+          (response: { refreshToken: string, token: string }) => {
+            this.setTokens(response.refreshToken, response.token)
+          },
+          () => this.removeTokens()
+        )
+      )
+      .toPromise();
+  }
+
+  setTokens(refreshToken: string, token: string, username?: string) {
+    localStorage.setItem('iot-ms-refresh-token', refreshToken);
+    localStorage.setItem('iot-ms-token', token);
+    this.isLoggedIn = true;
+
+    if (username) {
+      this.username = username;
+      localStorage.setItem('iot-ms-user', username);
+      this.announceLogIn(true);
+    }
+  }
+
+  removeTokens() {
+    localStorage.removeItem('iot-ms-refresh-token');
+    localStorage.removeItem('iot-ms-token');
+    localStorage.removeItem('iot-ms-user');
+    this.isLoggedIn = false;
+    this.announceLogIn(false);
+  }
+
+  hasExpired(token: string): boolean {
+    if (token) {
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+
+      const jsonPayload = decodeURIComponent(
+        atob(base64)
+          .split('')
+          .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+          .join('')
+      );
+
+      return JSON.parse(jsonPayload).exp - Date.now() / 1000 < 20;
+    }
+
+    return false;
   }
 
 }
