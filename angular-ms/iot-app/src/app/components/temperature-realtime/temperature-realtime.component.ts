03068526ef10056d5a27d4ca8db78d0f63de42fb
Rocky <rockygitt@gmail.com>
2020-04-29 02:43:44 +0200

    added specific micro realtime from URL

--- angular-ms/iot-app/src/app/components/temperature-realtime/temperature-realtime.component.ts
+++ angular-ms/iot-app/src/app/components/temperature-realtime/temperature-realtime.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
 
 import { ArduinoService } from '@services/arduino.service';
 
 import { Temperature } from '@models/temperature.model';
+import { Microcontroller } from '@models/microcontroller.model';
 
 @Component({
   selector: 'app-temperature-realtime',
@@ -12,7 +14,7 @@ import { Temperature } from '@models/temperature.model';
   templateUrl: './temperature-realtime.component.html'
 })
 export class TemperatureRealtimeComponent implements OnDestroy, OnInit {
-  H_AXIS_MAX: number = 10;
+  H_AXIS_MAX = 10;
   header: string[] = [ 'Tiempo', 'Temperatura' ];
   chart: GoogleChartInterface = {
     chartType: 'AreaChart',
@@ -29,20 +31,33 @@ export class TemperatureRealtimeComponent implements OnDestroy, OnInit {
     }
   };
   input: string;
-  refresh_time: number = 10000;
+  micro: Microcontroller;
+  refresh_time = 10000;
   interval: any;
-  checked: boolean = true;
+  checked = true;
   refresh_times: number[] = [ 5000, 10000, 30000, 60000 ];
 
-  constructor(private arduinoService: ArduinoService) { }
+  constructor(
+    private arduinoService: ArduinoService,
+    private route: ActivatedRoute
+  ) { }
 
   async ngOnInit() {
-    const temperature: Temperature = await this.arduinoService.getCurrentTemperature('192.168.1.50', 'temperature');
-    this.chart.dataTable = [
-      this.header,
-      [ new Date(temperature.date).toLocaleTimeString(), temperature.real_value ]
-    ];
-    this.interval = setInterval(() => this.getCurrentTemperature(), this.refresh_time);
+    const ip = this.route.snapshot.paramMap.get('ip');
+    const measure = this.route.snapshot.paramMap.get('measure');
+
+    try {
+      this.micro = await this.arduinoService.getMicrocontroller(ip, measure);
+
+      const temperature = await this.arduinoService.getCurrentTemperature(this.micro.ip, this.micro.measure);
+      this.chart.dataTable = [
+        this.header,
+        [ new Date(temperature.date).toLocaleTimeString(), temperature.real_value ]
+      ];
+      this.interval = setInterval(() => this.getCurrentTemperature(), this.refresh_time);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   ngOnDestroy() {
@@ -50,7 +65,7 @@ export class TemperatureRealtimeComponent implements OnDestroy, OnInit {
   }
 
   async getCurrentTemperature() {
-    const temperature: Temperature = await this.arduinoService.getCurrentTemperature('192.168.1.50', 'temperature');
+    const temperature = await this.arduinoService.getCurrentTemperature(this.micro.ip, this.micro.measure);
 
     if (this.chart.dataTable.length === this.H_AXIS_MAX + 1) {
       this.chart.dataTable.shift();
