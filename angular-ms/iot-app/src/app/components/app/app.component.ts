6959e889ae4135b8fd2333106e49455879bfc8e9
Rocky <rockygitt@gmail.com>
2020-06-15 01:03:19 +0200

    material sidebar

--- angular-ms/iot-app/src/app/components/app/app.component.ts
+++ angular-ms/iot-app/src/app/components/app/app.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core'
+import { MediaMatcher } from '@angular/cdk/layout'
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   styleUrls: [ './app.component.less' ],
   templateUrl: './app.component.html'
 })
-export class AppComponent { }
+export class AppComponent implements OnDestroy, OnInit {
+
+  ngOnInit() { }
+
+  mobileQuery: MediaQueryList
+  opened = false
+
+  toggle(opened: boolean) {
+    this.opened = !this.opened
+  }
+
+  private _mobileQueryListener: () => void
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)')
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges()
+    this.mobileQuery.addListener(this._mobileQueryListener)
+  }
+
+  ngOnDestroy() {
+    this.mobileQuery.removeListener(this._mobileQueryListener)
+  }
+
+  shouldRun = true
+}
