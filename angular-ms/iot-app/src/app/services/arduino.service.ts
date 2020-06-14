ef08ee08fa37405943a0409333bdbf1c8bb7c11a
Rocky <rockygitt@gmail.com>
2020-06-15 01:13:11 +0200

    new functions for all measures and micros

--- angular-ms/iot-app/src/app/services/arduino.service.ts
+++ angular-ms/iot-app/src/app/services/arduino.service.ts
@@ -7,6 +7,10 @@ import { tap } from 'rxjs/operators'
 import { environment } from 'src/environments/environment'
 
 import { Microcontroller } from '@models/microcontroller.model'
+import { Humidity } from '@models/humidity.model'
+import { HumidityStats } from '@models/humidity-stats.model'
+import { Light } from '@models/light.model'
+import { LightStats } from '@models/light-stats.model'
 import { Temperature } from '@models/temperature.model'
 import { TemperatureStats } from '@models/temperature-stats.model'
 
@@ -47,26 +51,44 @@ export class ArduinoService {
     }
   }
 
-  private getCurrentTemperatures(measure: string): Observable<Temperature[]> {
-    return this.http.get<Temperature[]>(`http://${environment.ORCHESTRATOR_MS}/${measure}`)
+  postMicrocontroller(microcontroller: Microcontroller): Observable<any> {
+    return this.http.post<any>(`http://${environment.ORCHESTRATOR_MS}/microcontrollers`, microcontroller)
   }
 
-  async getCurrentTemperature(ip: string, measure: string): Promise<Temperature> {
-    return new Promise<Temperature>(resolve => {
-      this.getCurrentTemperatures(measure)
-        .subscribe(temperatures => {
-          const filteredTemperatures = temperatures.filter(temperature => temperature.ip === ip)
-          resolve(filteredTemperatures.length ? filteredTemperatures[0] : null)
+  private getCurrentMeasures(measure: string): Observable<Humidity[] | Light[] | Temperature[]> {
+    return this.http.get<any[]>(`http://${environment.ORCHESTRATOR_MS}/${measure}`)
+  }
+
+  async getCurrentMeasure(ip: string, measure: string): Promise<Humidity | Light | Temperature> {
+    return new Promise<Humidity | Light | Temperature>(resolve => {
+      this.getCurrentMeasures(measure)
+        .subscribe(measures => {
+          const filteredMeasures = measures.filter(measure => measure && measure.ip === ip)
+          resolve(filteredMeasures.length ? filteredMeasures[0] : null)
         })
       })
   }
 
-  getPreviousTemperatures(ip: string, init_date: string, end_date: string): Observable<TemperatureStats[]> {
-    return this.http.get<TemperatureStats[]>(
-        `http://${environment.ORCHESTRATOR_MS}/temperature`,
+  async postLightStatus(ip: string, status: string): Promise<Light> {
+    return this.http.post<Light>(
+        `http://${environment.ORCHESTRATOR_MS}/light`,
+        { ip, status }
+      )
+      .toPromise()
+  }
+
+  getPreviousMeasures(
+    ip: string,
+    measure: string,
+    group: string,
+    init_date: string,
+    end_date: string
+  ): Observable<HumidityStats[] | LightStats[] | TemperatureStats[]> {
+    return this.http.get<HumidityStats[] | LightStats[] | TemperatureStats[]>(
+        `http://${environment.ORCHESTRATOR_MS}/${measure}`,
         { 
           params: {
-            path: 'temperatures',
+            path: group,
             ip,
             init_date,
             end_date
