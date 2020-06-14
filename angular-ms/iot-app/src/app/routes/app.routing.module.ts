dd70afb2ce3ba628b69e41833f922d1b226a469a
Rocky <rockygitt@gmail.com>
2020-06-15 01:13:24 +0200

    more routes

---
+++ angular-ms/iot-app/src/app/routes/app.routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+
+import { AuthGuard } from '@guards/auth.guard'
+
+import { DashboardComponent } from '@components/dashboard/dashboard.component'
+import { IndexComponent } from '@components/index/index.component'
+import { MicrocontrollersComponent } from '@components/microcontrollers/microcontrollers.component'
+
+const routes: Routes = [
+  { canActivate: [ AuthGuard ], component: DashboardComponent, path: 'dashboard' },
+  { canActivate: [ AuthGuard ], component: MicrocontrollersComponent, path: 'microcontrollers' },
+  { component: IndexComponent, path: '' },
+  // { path: '**', redirectTo: '' }
+]
+
+@NgModule({
+  exports: [
+    RouterModule
+  ],
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation: 'reload'
+    })
+  ]
+})
+export class AppRoutingModule { }
