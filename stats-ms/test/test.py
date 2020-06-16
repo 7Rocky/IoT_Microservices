a2c3643be2c50fc2f8173a299cfc11a1d0c9388c
Rocky <rockygitt@gmail.com>
2020-06-16 18:48:10 +0200

    Updated tests

--- stats-ms/test/test.py
+++ stats-ms/test/test.py
@@ -1,17 +1,63 @@
 import json
-import os
 import unittest
 
-from math import sqrt
-from src.measures.operations import mean, std
+from src.measures.humidity import Humidity
+from src.measures.light import Light
 from src.measures.temperature import Temperature
 
 class TestStatsMS(unittest.TestCase):
-    def test_mean(self):
-        self.assertEqual(mean([1, 2, 3, 4]), 2.5)
+    def test_humidity(self):
+        humidity = Humidity('humidities', 60)
+        self.assertEqual(humidity.queue, 'humidities')
+        self.assertEqual(humidity.max_items, 60)
 
-    def test_std(self):
-        self.assertEqual(std([1, 2, 3, 4]), sqrt(5) / 2)
+        with open('test/humidities.json', 'r') as f:
+            data = list(json.loads(f.read()))
+            self.assertEqual(
+                humidity.calculate_stats(data),
+                {
+                    'end_date': 'Sun, 26 Apr 2020 13:21:35 GMT',
+                    'end_timestamp': 1587907295530,
+                    'init_date': 'Sun, 26 Apr 2020 13:21:34 GMT',
+                    'init_timestamp': 1587907294530,
+                    'max_value': 40,
+                    'mean_value': 30.0,
+                    'measure': 'humidity',
+                    'ip': '192.168.1.50',
+                    'min_value': 20,
+                    'n_samples': 2,
+                    'real_values': [ 40, 20 ],
+                    'sensor': 'Grove - Moisture',
+                    'std_deviation': 14.1,
+                    'time_span': 1000,
+                    'username': 'Rocky'
+                }
+            )
+
+    def test_light(self):
+        light = Light('lights', 60)
+        self.assertEqual(light.queue, 'lights')
+        self.assertEqual(light.max_items, 60)
+
+        with open('test/lights.json', 'r') as f:
+            data = list(json.loads(f.read()))
+            self.assertEqual(
+                light.calculate_stats(data),
+                {
+                    'digital_values': [ 1, 0 ],
+                    'end_date': 'Sun, 26 Apr 2020 13:21:35 GMT',
+                    'end_timestamp': 1587907295530,
+                    'init_date': 'Sun, 26 Apr 2020 13:21:34 GMT',
+                    'init_timestamp': 1587907294530,
+                    'mean_value': 0.5,
+                    'measure': 'light',
+                    'ip': '192.168.1.50',
+                    'n_samples': 2,
+                    'sensor': 'Smart LED',
+                    'time_span': 1000,
+                    'username': 'Rocky'
+                }
+            )
 
     def test_temperature(self):
         temperature = Temperature('temperatures', 60)
@@ -35,7 +81,7 @@ class TestStatsMS(unittest.TestCase):
                     'n_samples': 2,
                     'real_values': [ 25.4, 22.4 ],
                     'sensor': 'Grove - Temperature',
-                    'std_deviation': 1.5,
+                    'std_deviation': 2.1,
                     'time_span': 1000,
                     'username': 'Rocky'
                 }
