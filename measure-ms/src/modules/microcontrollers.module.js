163c34130e1af0973f2d8dcf704dca24f4fd5add
Rocky <rockygitt@gmail.com>
2020-06-18 02:57:10 +0200

    Not caching micros (not using isInactive and ping)

--- measure-ms/src/modules/microcontrollers.module.js
+++ measure-ms/src/modules/microcontrollers.module.js
@@ -6,17 +6,6 @@ module.exports = class MicrocontrollersModule {
 
   constructor(measure) {
     this.measure = measure
-    this.microcontrollers = []
-  }
-
-  pingMicro = micro => {
-    const inactiveInterval = setInterval(async () => {
-      try {
-        await axios.get(`http://${micro.ip}/${micro.measure}`, { timeout: PING_TIMEOUT })
-        micro.isInactive = false
-        clearInterval(inactiveInterval)
-      } catch (error) { }
-    }, PING_TIMEOUT + 1000)
   }
 
   digitalToReal(digital, sensor) {
@@ -27,18 +16,12 @@ module.exports = class MicrocontrollersModule {
       case 'Grove - Temperature':
       case 'Fake Grove - Temperature':
         return Number((1 / (Math.log(1023 / digital - 1) / B_TERMISTOR + 1 / 298.15) - 273.15).toFixed(1))
-      default:
-        return Number((1 / (Math.log(1023 / digital - 1) / B_TERMISTOR + 1 / 298.15) - 273.15).toFixed(1))
     }
   }
 
   getMicrocontrollers = async () => {
-    if (this.microcontrollers && this.microcontrollers.length) return Promise.resolve(this.microcontrollers)
-
     const response = await axios.get(`http://${MICROCONTROLLERS_MS}/${this.measure}`)
-    this.microcontrollers = response.data
-    this.microcontrollers.forEach(micro => micro.isInactive = false)
-    return this.microcontrollers
+    return response.data
   }
 
 }
