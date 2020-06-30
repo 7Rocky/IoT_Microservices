e038f1133bc4a7580ea74f4452b058484a6e806f
Rocky <rockygitt@gmail.com>
2020-06-30 18:13:31 +0200

    fixed microcontrollers blue line

--- angular-ms/iot-app/src/app/components/dashboard-microcontroller/dashboard-microcontroller.component.ts
+++ angular-ms/iot-app/src/app/components/dashboard-microcontroller/dashboard-microcontroller.component.ts
@@ -9,10 +9,21 @@ import { Microcontroller } from '@models/microcontroller.model'
 })
 export class DashboardMicrocontrollerComponent implements OnInit {
 
+  @Input() isHistory: boolean = false
   @Input() micro: Microcontroller
 
   constructor() { }
 
   ngOnInit() { }
+
+  getMeasureView(name: string): string {
+    const measures: { name: string, view: string }[] = [
+      { name: 'humidity', view: 'Humedad' },
+      { name: 'light', view: 'Bombilla inteligente' },
+      { name: 'temperature', view: 'Temperatura' }
+    ]
+
+    return measures.filter(measure => measure.name === name)[0].view
+  }
 
 }
